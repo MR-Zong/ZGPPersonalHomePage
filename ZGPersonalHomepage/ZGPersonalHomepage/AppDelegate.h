885a83253f76a741f7b62535f000621e1f7fb5b1
ZongGenXu <540682674@qq.com>
2016-03-02 09:20:00 +0800

    init project

---
+++ ZGPersonalHomepage/ZGPersonalHomepage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZGPersonalHomepage
+//
+//  Created by Zong on 16/3/2.
+//  Copyright © 2016年 Zong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZGPersonalHomepage/ZGPersonalHomepage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZGPersonalHomepage
+//
+//  Created by Zong on 16/3/2.
+//  Copyright © 2016年 Zong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
